77636453262e6e393cf3a54e9aa87b5327a6eef9
stephanieraymos <stephanieraymos@gmail.com>
2022-03-20 17:44:48 -0700

    Moved css vars into js to manipulate them

--- Grid.js
+++ Grid.js
@@ -1,3 +1,11 @@
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+
 export default class Grid {
-    constructor(gridElement)
+  constructor(gridElement) {
+    gridElement.style.setProperty("--grid-size", GRID_SIZE);
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+  }
 }
